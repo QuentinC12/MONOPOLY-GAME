81d9c2f25bc9356dffb0c784a3c3b9cbfbc55919
QuentinC12 <57663979+QuentinC12@users.noreply.github.com>
2019-11-19 11:14:28 +0100

    Ajout de De.cs, Case.cs, Board.cs avec implementation des variabes de classe

---
+++ MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Case.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MONOPOLY_D_PATTERN
+{
+	class Case
+	{
+		private int statutCase; //Chance, prison, achetable ou déjà acheté
+		private int etatCase; //1 maison, 2 maison, 3 maison, hotel ou autre.
+		private string nomCase; // Nom exemple rue de la paix
+		private int[] Prix; //tableau de prix genre Prix[0] est sans maison, Prix[1] avec une maison
+	}
+}
--- MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Player.cs
+++ MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Player.cs
@@ -15,6 +15,30 @@ namespace MONOPOLY_D_PATTERN
 		private int statut;
 		private string username;
 
+		public Player()
+		{
+			nbPlayer++;
+			idPlayer = nbPlayer;
+			position = 0;
+			cash = 0;
+			statut = 0;
+			username = "unknow";
 
+		}
+		public Player(int cash, int statut, string username)
+		{
+			nbPlayer++;
+			idPlayer = nbPlayer;
+			position = 0;
+			this.cash = cash;
+			this.statut = statut;
+			this.username = username;
+
+		}
+
+		public override string ToString()
+		{
+			return "Joueur "+username+", A IMPLEMENTER ";
+		}
 	}
 }
