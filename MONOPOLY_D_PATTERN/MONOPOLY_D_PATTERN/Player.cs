f1680e9d3a62898cdb1d87c5df56b11fa4c8ce1c
QuentinC12 <57663979+QuentinC12@users.noreply.github.com>
2019-11-19 11:15:48 +0100

    Ajout de commentaires sur Player.cs

--- MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Player.cs
+++ MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Player.cs
@@ -8,16 +8,16 @@ namespace MONOPOLY_D_PATTERN
 {
 	class Player
 	{
-		private static int nbPlayer;
-		private int idPlayer;
-		private int position;
-		private int cash;
-		private int statut;
-		private string username;
+		private static int nbPlayer; //Nombre de player total -> Pour ça que static.
+		private int idPlayer; //Id du player
+		private int position; //position sur le plateau 0-39
+		private int cash; //Cash qu'à le joueur
+		private int statut; //Statut comme dit auparavant 4 -> 1er tour de prison ... 0 libre.
+		private string username; //Nom du joueur !
 
 		public Player()
 		{
-			nbPlayer++;
+			nbPlayer++;  
 			idPlayer = nbPlayer;
 			position = 0;
 			cash = 0;
