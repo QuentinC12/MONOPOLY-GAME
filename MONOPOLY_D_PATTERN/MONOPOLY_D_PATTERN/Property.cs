762b190b6ed013e2a0332cbd564e8b33a7d29a57
QuentinC12 <57663979+QuentinC12@users.noreply.github.com>
2019-11-19 13:03:10 +0100

    AJOUT DES SOUS CLASSES DE CASE, PASSAGE EN MODE ABSTRACT POUR CASE (CAR ON NE VEUT PAS d'INSTANCE DE CASE).

---
+++ MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Property.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MONOPOLY_D_PATTERN
+{
+	class Property : Case
+	{
+	private int etatCase; //1 maison, 2 maison, 3 maison, hotel ou autre.
+	private int idPlayerOwner; //mettre en -1 à la création| -1 = pas de owner / sinon hop l'id du owner
+	private int[] Prix; //tableau de prix genre Prix[0] est sans maison, Prix[1] avec une maison
+	}
+}
