d56dad0f5fdacdeaeacfab85b9bcd0ed409f84f5
QuentinC12 <57663979+QuentinC12@users.noreply.github.com>
2019-11-19 11:45:02 +0100

    Modification de De.cs avec ajout d'idées. A réfléchir -> le nbDeLancésSuccessifs à mettre dans la classe De ou dans les variables de classe de Player ?

--- MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/De.cs
+++ MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/De.cs
@@ -8,5 +8,22 @@ namespace MONOPOLY_D_PATTERN
 {
 	class De
 	{
+		//DE 1 et D2;
+		//Nombre de lancés successifs
+
+		public void initiateLaunch()
+		{
+			//nb de lancés successifs = 0;
+		}
+
+		public int lancerDe()
+		{
+			return 0;
+		}
+
+		public bool SameValue()
+		{
+			return false;
+		}
 	}
 }
