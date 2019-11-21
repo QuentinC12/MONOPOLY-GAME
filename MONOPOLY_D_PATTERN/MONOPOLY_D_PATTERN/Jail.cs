8d3032ff5a62114b3ce667e23c332982b01b2fa1
lucie <lucie.dugat@edu.devinci.fr>
2019-11-21 15:01:18 +0100

    Updated child class Jail

--- MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Jail.cs
+++ MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Jail.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MONOPOLY_D_PATTERN
+namespace MONOPOLY_D_PATTERN.Properties
 {
-	class Jail : Case
-	{
-	}
+    class Jail : Case
+    {
+        public Jail(int id, string stateCase, string name):base(id, stateCase,name)
+        {
+        }
+        public override int Id
+        {
+            get
+            {
+                return this.id;
+            }
+        }
+        public override string StateCase
+        {
+            get { return this.StateCase; }
+        }
+        public override string NameCase
+        {
+            get { return this.nameCase; }
+        }
+    }
 }
