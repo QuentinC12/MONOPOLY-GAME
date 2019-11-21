67805647350e576084724f01d436b4241c08f634
lucie <lucie.dugat@edu.devinci.fr>
2019-11-21 14:27:54 +0100

    Added constructor in abstract Case class

--- MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Case.cs
+++ MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Case.cs
@@ -8,11 +8,16 @@ namespace MONOPOLY_D_PATTERN
 {
 	abstract class Case
 	{
-		private int id;
-		private int statutCase; //Chance, communauté, prison, achetable ou déjà acheté
-		private string nomCase; // Nom exemple rue de la paix
+		protected int id;
+		protected string stateCase; //Chance, communauté, prison, (achetable ou déjà acheté ça non ça sera ailleurs) 
+		protected string nameCase; // Nom exemple rue de la paix
 		
-
+        public Case(int id,string stateCase, string name)
+        {
+            this.id = id;
+            this.stateCase=stateCase;
+            this.nameCase = name;
+        }
 		//SOIT ON L'INITIALISE AVEC UN CSV NOUS DONNANT CHAQUE CASE SOIT ON REMPLIT EN DUR CHAQUE CASE.
 
 		//FAIRE LES GETTER AND SETTER
