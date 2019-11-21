769e12c6e1de6a87734e1862b0b0848331a10b3f
lucie <lucie.dugat@edu.devinci.fr>
2019-11-21 15:05:46 +0100

    Created a class Case for cases that do nothing for now (Train station, start ...)

---
+++ MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/OtherCases.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace MONOPOLY_D_PATTERN
+{   //Cases without specifities for now
+    class OtherCases : Case
+    {
+        public OtherCases(int id, string stateCase, string name) :base(id,stateCase,name)
+        {
+        }
+        public override int Id
+        {
+            get
+            {
+                return this.id;
+            }
+        }
+        public override string StateCase
+        {
+            get { return this.StateCase; }
+        }
+        public override string NameCase
+        {
+            get { return this.nameCase; }
+        }
+    }
+}
