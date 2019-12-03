18a4dd110023db52a90a283df014a66b51adbeb7
luciedug <43863738+luciedug@users.noreply.github.com>
2019-12-03 15:54:16 +0100

    Changement des certains attributs de Case à Property

--- MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Case.cs
+++ MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Case.cs
@@ -11,17 +11,13 @@ namespace MONOPOLY_D_PATTERN
 		protected int id;
 		protected string stateCase; //Chance, communauté, prison, (achetable ou déjà acheté ça non ça sera ailleurs) 
 		protected string nameCase; // Nom exemple rue de la paix
-		private bool owned;
-		private int idOwner;
-		private int nbHouse;
+		
 		public Case(int id,string stateCase, string name)
         {
             this.id = id;
             this.stateCase=stateCase;
             this.nameCase = name;
-			this.owned = false;
-			this.idOwner =-1;
-			this.nbHouse = 0;
+			
 		}
 		
 
@@ -29,22 +25,7 @@ namespace MONOPOLY_D_PATTERN
         public abstract string StateCase { get; }
         public abstract string NameCase { get; }
 
-		public bool Owned
-		{
-			get { return owned; }
-			set { owned = value; }
-		}
-		public int IDOwner
-		{
-			get { return idOwner; }
-			set { idOwner = value; }
-		}
-
-		public int NbHouse
-		{
-			get { return nbHouse; }
-			set { nbHouse = value; }
-		}
+		
 
 		 
 
--- MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Property.cs
+++ MONOPOLY_D_PATTERN/MONOPOLY_D_PATTERN/Property.cs
@@ -11,14 +11,19 @@ namespace MONOPOLY_D_PATTERN
         private int etatCase; //O rien 1 maison, 2 maison, 3 maison, hotel ou autre.
         private int idPlayerOwner; //mettre en -1 à la création| -1 = pas de owner / sinon hop l'id du owner
         private int[] prix; //tableau de prix genre Prix[0] est sans maison, Prix[1] avec une maison
-		
+        private bool owned;
+        private int idOwner;
+        private int nbHouse;
 
         public Property(int id, string stateCase, string name, int[] prix) : base(id, stateCase, name)
         {
             this.etatCase = 0;
             this.idPlayerOwner = -1;
             this.prix = prix;
-			
+            this.owned = false;
+            this.idOwner = -1;
+            this.nbHouse = 0;
+
         }
         public override int Id
         {
@@ -49,11 +54,27 @@ namespace MONOPOLY_D_PATTERN
         {
             get { return this.prix; }
         }
+        public bool Owned
+        {
+            get { return owned; }
+            set { owned = value; }
+        }
+        public int IDOwner
+        {
+            get { return idOwner; }
+            set { idOwner = value; }
+        }
 
-		
+        public int NbHouse
+        {
+            get { return nbHouse; }
+            set { nbHouse = value; }
+        }
 
 
 
-	}
+
+
+    }
 }
 
